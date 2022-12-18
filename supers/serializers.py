3ead48e2b2730ea8ae3dbe2990d4c38b55d257f7
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-18 16:47:11 -0500

    updated Serializers.py in supers

---
+++ supers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Super
+
+class SuperSerializer(ModelSerializer):
+    class Meta:
+        model = Super
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase',]
