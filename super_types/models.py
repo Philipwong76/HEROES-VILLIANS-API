cf38fd69d70914f310d058362453180a09f1703c
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-18 16:20:24 -0500

    Created SuperType model in super_type app and Created Super model in super app

--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class SuperType(models.Model):
+    type = models.CharField(max_length=255)
--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from super_types.models import SuperType
 
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
