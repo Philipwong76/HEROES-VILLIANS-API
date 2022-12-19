f1cde4296bcb70c27f67cd9dd2d35baeeb62c9b6
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-18 23:02:01 -0500

    Created view function to update specific hero or villain to update on supers app

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,13 +17,14 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def super_detail(request, pk):
-    try:
-        super = Super.objects.get(pk=pk)
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
         serializer = SuperSerializer(super);
         return Response(serializer.data)
-        
-    except Super.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND);
-    
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
