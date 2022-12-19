0b54cce4d867fd161a43348e1d390e9172a9987c
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-18 22:28:15 -0500

    Updated supers views.py with 'GET' and 'POST' view_api requests + added admin function for supers

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    
-
-    return Response('ok')
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
