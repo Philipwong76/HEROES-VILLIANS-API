a3cc642e75ee2c9c2a5d447111fa7db82e215933
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-18 23:06:21 -0500

    completed DELETE function for super view.py

--- supers/views.py
+++ supers/views.py
@@ -17,7 +17,7 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
     super = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
@@ -28,3 +28,6 @@ def super_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
