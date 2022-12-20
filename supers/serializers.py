cf95dbf8e13737d2ddf39f9f047ca636b8f91c35
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-20 18:57:39 -0500

    Corrected errors made in heroes_villains_project/urls.py and supers/serializers.py

--- supers/serializers.py
+++ supers/serializers.py
@@ -4,4 +4,4 @@ from .models import Super
 class SuperSerializer(ModelSerializer):
     class Meta:
         model = Super
-        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase',]
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
