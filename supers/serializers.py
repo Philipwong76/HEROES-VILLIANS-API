e8e3dc13f764bcfcc72c9a00298b442cf3fb8a2a
Philip Wong <110500922+ThatBoiisback@users.noreply.github.com>
2022-12-20 20:22:33 -0500

    Updated super serializers.py + created supers view.py functions to select types of supertypes.

--- supers/serializers.py
+++ supers/serializers.py
@@ -5,3 +5,4 @@ class SuperSerializer(ModelSerializer):
     class Meta:
         model = Super
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        depth = 1
--- supers/views.py
+++ supers/views.py
@@ -8,8 +8,16 @@ from .models import Super
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        supers = Super.objects.all()
-        serializer = SuperSerializer(supers, many=True)
+
+        super_type_type = request.query_params.get('type')
+        print(super_type_type)
+
+        queryset = Super.objects.all()
+
+        if super_type_type:
+            queryset = queryset.filter(super_type__type=super_type_type)
+
+        serializer = SuperSerializer(queryset, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
